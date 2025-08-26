87c246a35f18e0b7965888322caf8ad4b21e4a61
Emicora <emicoratolo2501@gmail.com>
2025-08-26 17:15:07 -0300

    primer commit

--- composeApp/src/commonMain/kotlin/org/basic/project/App.kt
+++ composeApp/src/commonMain/kotlin/org/basic/project/App.kt
@@ -22,5 +22,20 @@ import kmpbasico.composeapp.generated.resources.compose_multiplatform
 @Composable
 fun App() {
     MaterialTheme {
+        Column(
+            modifier = Modifier
+                .background( color = MaterialTheme.colorScheme.primaryContainer)
+                .safeContentPadding()
+                .fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Hola mundo!!!"
+            )
+            Text(
+                text = "Programacion 2"
+            )
+        }
     }
+
 }
